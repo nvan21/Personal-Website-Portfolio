6c79063e22e4217b56792a8da711c59282b622e2
nvan21 <153444401+nvan21@users.noreply.github.com>
2024-08-07 18:01:47 -0500

    Tried getting rid of base from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/personal-portfolio-v2/",
+  // base: "/personal-portfolio-v2/",
 });
