980fc3b3fbdb54b8a698242306c7befe2c21b371
nvan21 <153444401+nvan21@users.noreply.github.com>
2024-08-07 19:40:57 -0500

    Added dist to commits

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/personal-portfolio-v2/",
-  build: {
-    outDir: "dist", // Ensure this is set correctly
-  },
 });
