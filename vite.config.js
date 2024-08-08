9bc52e8d17b83707e48aacf19e813074af3d0fd9
nvan21 <153444401+nvan21@users.noreply.github.com>
2024-08-07 19:24:53 -0500

    Added outDir to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/personal-portfolio-v2/",
+  build: {
+    outDir: "dist", // Ensure this is set correctly
+  },
 });
